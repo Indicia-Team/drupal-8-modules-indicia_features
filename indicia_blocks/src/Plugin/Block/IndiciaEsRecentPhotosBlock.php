683bd3e0accbecba5a73e3d7e477150e8807edff
John van Breda <john.vanbreda@biodiverseit.co.uk>
2024-08-08 10:14:32 +0100

    Photos block fixes and uses horizontal layout

--- indicia_blocks/src/Plugin/Block/IndiciaEsRecentPhotosBlock.php
+++ indicia_blocks/src/Plugin/Block/IndiciaEsRecentPhotosBlock.php
@@ -9,7 +9,7 @@ use Drupal\Core\Render\Markup;
  * Provides a 'Recent Elasticsearch photos' block.
  *
  * @Block(
- *   id = "es_recent_photos",
+ *   id = "es_recent_photos_block",
  *   admin_label = @Translation("Recent Elasticsearch photos block"),
  * )
  */
@@ -79,7 +79,8 @@ class IndiciaEsRecentPhotosBlock extends IndiciaBlockBase {
     ]);
     $r .= \ElasticsearchReportHelper::cardGallery([
       'id' => 'photo-cards-' . self::$blockCount,
-      'source' => 'es-photos- ' . self::$blockCount,
+      'class' => 'horizontal',
+      'source' => 'es-photos-' . self::$blockCount,
       'columns' => [
         [
           'field' => '#taxon_label#',
